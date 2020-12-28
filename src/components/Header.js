f4328d73291ef9413eb98a67fa3e64e3b096460e
vijay <talviyav@gmail.com>
2020-12-28 14:29:28 -0500

    typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import {NavLink} from "react-router-dom";
 const Header=()=>(
     <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="on-active" exact={true}>Deshboard</NavLink>
+    <NavLink to="/" activeClassName="on-active" exact={true}>Dashboard</NavLink>
     <NavLink to="/create" activeClassName="on-active">Create Expense</NavLink>
     <NavLink to="/help" activeClassName="on-active">Help</NavLink>
     </header>
